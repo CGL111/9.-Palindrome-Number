0f6846487821a91728fd0fc22875ffb6b6bc0aae
CGL111 <33282519+CGL111@users.noreply.github.com>
2018-02-07 18:51:28 +0800

    Create Solution.c

---
+++ Solution.c
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int n = 0 ,s;
+       if(x<0) return false;
+        s = x;
+      
+        while(s > 0){
+          s/=10;
+          n++;
+          
+        } 
+         
+        for(int m = 1;m <= n/2;m++){
+          
+            if( !((x%(int)pow(10,m)/(int)pow(10,m-1)) == (x%(int)pow(10,n+1-m)/(int)pow(10,n-m)))) return false;
+        }
+        return true;
+    }
+};
